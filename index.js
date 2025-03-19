5e1a6c643d57e229a0e940d851198e6cca878ec9
Suyog Bhatia <79241565+suyogbhatia@users.noreply.github.com>
2025-03-19 23:58:37 +0530

    added socket io in frontEnd and backend

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express= require('express');
 const app = express();
-const socket= require('socket.io');
+const {Server}= require('socket.io');
 const http = require('http');
 
 const server = http.createServer(app);
+const io = new Server(server);
 
+io.on('connection',(socket)=>{
+    socket.on('message', (msg)=>{
+        io.emit('message', msg);
+    })
+})
 app.get('/', (req, res)=>{
     res.sendFile(__dirname +"/index.html");
 })

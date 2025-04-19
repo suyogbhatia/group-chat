47fbff23cf2d3bcac785fab7b7aed3016fe591e5
Suyog Bhatia <79241565+suyogbhatia@users.noreply.github.com>
2025-04-19 11:40:35 +0530

    changed cjs to esm

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-This project is using Node.js, express and socket.io for a group chat.
+# Group Chat application
+
+A simple Node.js application, using express and socket.io to implement websockets for a group chat.
+Uses latest node version v22.14.0
+
+# To install dependencies
+
+run `npm i` in root folder
+
+# To run
+
+simply execute `node index.js` to run the application in local
+To run while developing use `npm run start:dev` after installing nodemon
+
+Application will be served on `http://localhost:3000/`
--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-const express= require('express');
+import express from 'express';
+// const express= require('express');
+
 const app = express();
-const {Server}= require('socket.io');
-const http = require('http');
+import * as socket from 'socket.io';
+import * as http from 'http';
+import path from 'path';
+// const {Server}= require('socket.io');
+// const http = require('http');
 
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new socket.Server(server);
 
 io.on('connection',(socket)=>{
     socket.on('message', (msg)=>{
         io.emit('message', msg);
     })
 })
+app.use(express.static(path.resolve()+'/images'));
 app.get('/', (req, res)=>{
-    res.sendFile(__dirname +"/index.html");
+    console.log(path.resolve())
+    res.sendFile(path.resolve()+'/index.html');
 })
 
 server.listen(3000, ()=> {
